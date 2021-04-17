3e970eb3f1b4545991ece2d2df5136e7574590b0
shameera17 <shameera.carrim17@gmail.com>
2021-04-17 14:54:21 +0530

    add change

--- src/components/SaveChangesModel/SaveChanges.js
+++ src/components/SaveChangesModel/SaveChanges.js
@@ -7,6 +7,7 @@ function SaveChanges() {
     return (
         <div>
            <h1>Kaveen</h1>
+           <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam, praesentium?</h1>
         </div>
     )
 }
